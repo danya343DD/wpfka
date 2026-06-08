276930344ca4dc133a7c8804af80d0b85524d1ab
danya343DD <stalinovic45@gmail.com>
2026-06-08 18:13:09 +0900

    Add files via upload

---
+++ buttonswe/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] colors = {
+                "Navy", "Blue", "Aqua", "Teal", "Olive", "Green",
+                "Lime", "Yellow", "Orange", "Red", "Maroon",
+                "Fuchsia", "Purple", "Black", "Silver", "Gray", "White"
+            };
+
+            ColorButtonsList.ItemsSource = colors;
+        }
+    }
+}
